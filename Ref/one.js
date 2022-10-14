baa3f297bd859735e6b2839a3f3526f50a9611c8
raghava ruby <viecho #>
2022-10-14 11:03:55 +0530

    ok

---
+++ Ref/one.js
@@ -0,0 +1,12 @@
+function display(){
+
+
+    
+   console.log(document.getElementById('abc').innerHTML) 
+
+   console.log(document.getElementById('name').value)
+
+   let btnref = document.getElementById('xyz')
+   //console.log(btnref)
+   btnref.disabled = false
+}
